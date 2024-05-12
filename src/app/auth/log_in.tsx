8f850b31cd32f9d525e8e67ec90007f15c0e9fd3
Blau <62470333+blau-1996@users.noreply.github.com>
2024-05-12 16:24:31 +0900

    Setting hooks

--- src/app/auth/log_in.tsx
+++ src/app/auth/log_in.tsx
@@ -1,22 +1,47 @@
 import { View, Text, TextInput, TouchableOpacity, StyleSheet } from "react-native"
+import { Link, router } from "expo-router"
+import { useState } from "react"
 
-import Header from "../../components/Header"
 import Button from "../../components/button"
 
+const handlePress = (): void => {
+  // ログイン
+  router.replace("/memo/list")
+}
+
 const LogIn = (): JSX.Element => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
   return (
     <View style={styles.container}>
-      <Header />
       <View style={styles.inner}>
         <Text style={styles.title}>Log In</Text>
-        <TextInput style={styles.input} value="Email address" />
-        <TextInput style={styles.input} value="Password" />
-        <Button label="Submit" />
+        <TextInput
+          style={styles.input}
+          value={email}
+          onChangeText={(text) => { setEmail(text) }}
+          autoCapitalize="none"
+          keyboardType="email-address"
+          placeholder="Email Address"
+          textContentType="emailAddress"
+        />
+        <TextInput
+          style={styles.input}
+          value={password}
+          onChangeText={(text) => { setPassword(text) }}
+          autoCapitalize="none"
+          secureTextEntry
+          placeholder="Password"
+          textContentType="password"
+        />
+        <Button label="Submit" onPress={handlePress} />
         <View style={styles.footer}>
           <Text style={styles.footerText}>Not registered?</Text>
-          <TouchableOpacity>
-            <Text style={styles.footerLink}>Sign up here!</Text>
-          </TouchableOpacity>
+          <Link href="/auth/sign_up" asChild>
+            <TouchableOpacity>
+              <Text style={styles.footerLink}>Sign up here!</Text>
+            </TouchableOpacity>
+          </Link>
         </View>
       </View>
     </View>
