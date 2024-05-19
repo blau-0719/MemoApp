88387cc7b701b64f2729e54b68c8e86cdaceaf0d
Blau <62470333+blau-1996@users.noreply.github.com>
2024-05-19 17:59:31 +0900

    Implement edit screen

--- src/app/memo/detail.tsx
+++ src/app/memo/detail.tsx
@@ -8,17 +8,16 @@ import Icon from "../../components/icon"
 import { auth, db } from "../../config"
 import type { Memo } from "../../../types/memo"
 
-const handlePress = (): void => {
-  router.push("memo/edit")
+const handlePress = (id: string): void => {
+  router.push({ pathname: "memo/edit", params: { id }})
 }
 
 const Detail = (): JSX.Element => {
-  const { id } = useLocalSearchParams()
-  console.log(id)
+  const id = String(useLocalSearchParams().id)
   const [memo, setMemo] = useState<Memo | null>(null)
   useEffect(() => {
     if (!auth.currentUser) { return }
-    const ref = doc(db, `users/${auth.currentUser.uid}/memos`, String(id))
+    const ref = doc(db, `users/${auth.currentUser.uid}/memos`, id)
     const unsubscribe = onSnapshot(ref, (memoDoc) => {
       const { bodyText, updatedAt } = memoDoc.data() as Memo
       setMemo({
@@ -40,7 +39,7 @@ const Detail = (): JSX.Element => {
           {memo?.bodyText}
         </Text>
       </ScrollView>
-      <CircleButton onPress={handlePress} style={{ top: 60, bottom: 'auto' }}>
+      <CircleButton onPress={() => { handlePress(id) }} style={{ top: 60, bottom: 'auto' }}>
         <Icon name='pencil' size={40} color='#ffffff'/>
       </CircleButton>
     </View>
